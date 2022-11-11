0ed1b0dee09555496706457814f5c40799588a3e
Jay Kim <30487743+kinoasm@users.noreply.github.com>
2022-11-11 22:38:59 +0900

    [level 3] Title: 최적의 행렬 곱셈, Time: 42.51 ms, Memory: 32.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12942. 최적의 행렬 곱셈/최적의 행렬 곱셈.js
@@ -0,0 +1,26 @@
+function solution(m) {
+    var answer = 0;
+    var key=[];
+    var len=m.length
+    for(var i in m){
+        key.push(m[i][0])
+    }
+    key.push(m[len-1][1])
+    var arr=[];
+    for(var i=0;i<len;i++){
+        var row=[]
+        for(var j=0;j<len;j++){
+            row.push(0)
+        }
+        arr.push(row)
+    }
+    for(var s=1;s<=len-1;s++){
+        for(var i=0;i<=len-s-1;i++){
+            for(var k=i;k<=i+s-1;k++){
+                if(k==i)arr[i][i+s]=arr[i][k]+arr[k+1][i+s]+key[i]*key[k+1]*key[i+s+1];
+                arr[i][i+s]=Math.min(arr[i][i+s],arr[i][k]+arr[k+1][i+s]+key[i]*key[k+1]*key[i+s+1]);
+            }
+        }
+    }
+    return arr[0][len-1];
+}
