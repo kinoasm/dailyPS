881930dfc40b1d54c88d88bf329c28cd100efbb4
Jay Kim <30487743+kinoasm@users.noreply.github.com>
2022-12-30 15:12:47 +0900

    [Bronze II] Title: Hashing, Time: 144 ms, Memory: 9588 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.js
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -1,42 +1,14 @@
-let arr = require("fs")
-    .readFileSync("/dev/stdin")
-    .toString()
-    .trim()
-    .split("\n")
-    .map((x) => x.trim());
-let [n] = arr.slice(0, 1)[0].split(" ").map(Number);
-str = arr.slice(1)[0];
-const solution = (n, str) => {
-    let powList = [31];
-    while (powList.length < 8)
-        powList.push(
-            (powList[powList.length - 1] * powList[powList.length - 1]) %
-                1234567891
-        );
-    let remList = [1, 31];
-    const to2 = (int) => {
-        let res = [];
-        while (int > 0) {
-            res.push(int % 2);
-            int = Math.floor(int / 2);
-        }
-        return res;
-    };
-    for (let i = 2; i <= 50; i++) {
-        let ar = to2(i);
-        let now = 1;
-        for (let j in ar) {
-            if (ar[j]) now = (now * powList[j]) % 1234567891;
-        }
-        remList.push(now);
-    }
-    let arr = str.split("");
-    let res = 0;
-    for (let i = 0; i < arr.length; i++) {
-        let x = (arr[i].charCodeAt(0) - 96) * remList[i];
-        res = (res + x)%1234567891;
-    }
-    return res;
-};
-
-console.log(solution(n, str));
+const [n, str] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((x) => x.trim());
+const multi = [1, 31];
+while (multi.length < 50) multi.push((multi.at(-1) * 31) % 1234567891);
+let hash = 0;
+for (let i = 0; i < str.length; i++)
+  hash =
+    (hash + (((str[i].charCodeAt(0) - 96) * multi[i]) % 1234567891)) %
+    1234567891;
+console.log(hash);
